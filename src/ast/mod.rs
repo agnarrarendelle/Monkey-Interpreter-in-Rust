7c39f580b307efb56e0d0c01ed194b9732097405
agnarrarendelle <danny20001102@gmail.com>
2022-11-05 09:52:10 +0800

    .

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,55 +1,73 @@
-use std::fmt::{Display, self};
+use std::fmt::{self, Display};
 
 #[derive(Debug)]
-pub enum Node{
+pub enum Node {
     Stat(Statement),
-    Expr(Expression)
+    Expr(Expression),
 }
 
 #[derive(Debug)]
-pub struct Program{
-    pub statements: Vec<Statement>
+pub struct Program {
+    pub statements: Vec<Statement>,
 }
-
 
 impl Default for Program {
     fn default() -> Self {
-        Self{
-            statements:vec![]
-        }
+        Self { statements: vec![] }
     }
 }
 
-
 #[derive(Debug)]
-pub enum Statement{
+pub enum Statement {
     Let(String, Expression),
-    Return(Expression)
+    Return(Expression),
+    Expression(Expression),
 }
 
-impl fmt::Display for Statement{
+impl fmt::Display for Statement {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self{
-            Statement::Let(identifier,expression )=>{
-                return  write!(f, "let {} = {};", identifier, expression);
-            },
-            Statement::Return(expression)=>{
-                return  write!(f, "return {};", expression);
+        match self {
+            Statement::Let(identifier, expression) => {
+                return write!(f, "let {} = {};", identifier, expression);
+            }
+            Statement::Return(expression) => {
+                return write!(f, "return {};", expression);
+            }
+            Statement::Expression(expression) => {
+                return write!(f, "{}", expression);
             }
         }
     }
 }
 #[derive(Debug)]
-pub enum Expression{
-    Identifier(String)
+pub enum Expression {
+    Identifier(String),
 }
 
-impl fmt::Display for Expression{
+impl fmt::Display for Expression {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self{
-            Expression::Identifier(identifier)=>{
-                return  write!(f, "{}", identifier);
+        match self {
+            Expression::Identifier(identifier) => {
+                return write!(f, "{}", identifier);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fmt::write;
+
+    use super::*;
+
+    #[test]
+    fn test_string() {
+        let letStatement = Statement::Let(
+            "myVar".to_string(),
+            Expression::Identifier("anotherVar".to_string()),
+        );
+
+        let result = format!("{}", letStatement);
+        assert_eq!(result, "let myVar = anotherVar;");
     }
 }
