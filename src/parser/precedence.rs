88094a1f07442333afb1191d8677f32fcef519ed
agnarrarendelle <danny20001102@gmail.com>
2022-11-08 23:00:23 +0800

    added a function that transform a token to its corresponding Precedence

--- src/parser/precedence.rs
+++ src/parser/precedence.rs
@@ -1,3 +1,6 @@
+use crate::token::Token;
+
+#[derive(PartialEq, PartialOrd)]
 pub enum Precedence {
     LOWEST,
     EQUALS,      // ==
@@ -6,4 +9,14 @@ pub enum Precedence {
 	PRODUCT,     // *
 	PREFIX,      // -X or !X
 	CALL        // myFunction(X)
+}
+
+pub fn token_to_precedence(token: &Token)->Precedence{
+    match  token {
+        Token::EQ | Token::NOTEQ=>Precedence::EQUALS,
+        Token::GT | Token::LT=>Precedence::LESSGREATER,
+        Token::PLUS | Token::MINUS=>Precedence::SUM,
+        Token::SLASH | Token::ASTERISK=>Precedence::PRODUCT,
+        _=>Precedence::LOWEST
+    }
 }
