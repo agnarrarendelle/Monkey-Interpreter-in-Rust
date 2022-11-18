c79b80ef084df668ea84f09faff24adba23f7b1d
agnarrarendelle <danny20001102@gmail.com>
2022-11-19 00:43:18 +0800

    modified the error module so that now it can generate error message for different type of errors

--- src/evaluator/error.rs
+++ src/evaluator/error.rs
@@ -1,13 +1,42 @@
-pub struct EvalError(String);
+// pub struct EvalError(String);
 
-impl fmt::Display for EvalError{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        return write!(f, "{}", self.0);
-    }
+// impl fmt::Display for EvalError{
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         return write!(f, "Error: {}", self.0);
+//     }
+// }
+
+// impl EvalError{
+//     pub fn new(err: String)->Self{
+//         Self(err)
+//     }
+
+//     pub fn unknown_operator_error()
+// }
+
+use std::fmt;
+
+use crate::{object::Object, token::Token};
+
+pub enum EvalError {
+    UnknownOperatorError
 }
 
-impl EvalError{
-    pub fn new(err: String)->Self{
-        Self(err)
+enum UnknownOperatorError<'a> {
+    Prefix(&'a Token, &'a Object),
+    MinusPrefix(&'a Object),
+    IntegerBooleanPrefix(&'a i64, &'a Token, &'a i64),
+}
+impl fmt::Display for UnknownOperatorError<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let res = match self {
+            UnknownOperatorError::Prefix(t, o) => format!("{}{}", t, o),
+            UnknownOperatorError::MinusPrefix(o) => format!("-{}", o),
+            UnknownOperatorError::IntegerBooleanPrefix(left, t, right) => {
+                format!("{} {} {}", left, t, right)
+            }
+        };
+
+        write!(f, "{}", res)
     }
 }
