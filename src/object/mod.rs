d1d979ac701f2b15093c12fc68b43398a9c7345f
agnarrarendelle <danny20001102@gmail.com>
2022-11-18 21:13:33 +0800

    added a ReturnValue to enum as a wrapper that wraps around return values from expressions to be used later

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,9 +1,10 @@
-use std::fmt;
+use std::{fmt, rc::Rc};
 
 #[derive(PartialEq, Debug)]
 pub enum Object {
     Integer(i64),
     Boolean(bool),
+    ReturnValue(Rc<Object>),
     Null
 }
 
@@ -12,6 +13,7 @@ impl fmt::Display for Object {
         match self {
             Object::Integer(i) => write!(f, "{}", i),
             Object::Boolean(b) => write!(f, "{}", b),
+            Object::ReturnValue(v)=> write!(f, "{}", v),
             Object::Null => write!(f, "NULL"),
         }
     }
