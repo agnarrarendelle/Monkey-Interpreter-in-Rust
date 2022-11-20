41730f1d74b2501fda1ba38923e86054aadc9892
agnarrarendelle <danny20001102@gmail.com>
2022-11-20 21:49:19 +0800

    added a module that manages all identifiers and their associated values

---
+++ src/object/environment.rs
@@ -0,0 +1,22 @@
+use std::{collections::HashMap, rc::Rc, cell::RefCell};
+
+use super::Object;
+
+pub type Env = Rc<RefCell<Environment>>; 
+
+pub struct Environment{
+    store: HashMap<String, Rc<Object>>
+}
+
+impl Environment{
+    pub fn get(&self,name: &str)->Option<Rc<Object>>{
+        match self.store.get(name){
+            Some(obj)=>Some(Rc::clone(obj)),
+            None=>None
+        }
+    }
+
+    pub fn set(&mut self, name:&str, object:Rc<Object>){
+        self.store.insert(name.to_string(), object);
+    }
+}
